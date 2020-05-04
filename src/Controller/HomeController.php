8482420301aba860a7c36a2199fede2073f3145e
Hermoult <adrienhergat@gmail.com>
2020-05-04 13:45:24 +0200

    Rendu 1

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,10 +1,12 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
-class HomeController{
+class HomeController extends AbstractController{
     
     /**
      *
@@ -16,9 +18,13 @@ class HomeController{
 
         $this->twig = $twig;
     }
-    
+        
+    /**
+     * @Route ("/", name="home")
+     * @return Response
+     */
     public function index(): Response{
 
-        return new Response ($this->twig->render('pages/home.html.twig'));
+        return $this->render('pages/home.html.twig');
     }
 }
--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -1,17 +1,18 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class PropertyController{
+class PropertyController extends AbstractController{
     
     /**
      * @Route("/biens", name="property.index")
      * @return Response
      */
-    public function index(): Response{
-        return new response("les biens");
-        
+    public function index(): Response
+    {
+        return $this->render('property/index.html.twig', ['current_menu' => 'properties']);     
     }
 }
